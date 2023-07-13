bed4e62719a4b1f1d8131524ccfc0344842ea989
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-14 00:30:40 +0900

    July.13.2023
    Business Layer 테스트(1)
    
    Persistence Layer
    - Data Access의 역할
    - 비지니스 가공 로직이 포함되어서는 안 된다. Data에 대한 CRUD에만 집중한 레이어
    
    Business Layer
    - 비지니스 로직을 구현하는 역할
    - Persistence Layer와의 상호작용(Data를 읽고 쓰는 행위)을 통해 비지니스 로직을 전개시킨다.
    - 트랜잭션을 보장해야 한다.
    
    요구사항
    - 상품 번호 리스트를 받아 주문 생성하기
    - 주문은 주문 상태, 주문 등록 시간을 가진다.
    - 주문의 총 금액을 계산할 수 있어야 한다.

---
+++ src/main/java/sample/cafekiosk/spring/api/controller/order/OrderController.java
@@ -0,0 +1,26 @@
+package sample.cafekiosk.spring.api.controller.order;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import sample.cafekiosk.spring.api.service.order.OrderService;
+import sample.cafekiosk.spring.api.service.order.request.OrderCreateRequest;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping("/api/v1/orders/new")
+    public void createOrder(@RequestBody OrderCreateRequest request) {
+        LocalDateTime registeredDateTime =  LocalDateTime.now();
+        orderService.createOrder(request, registeredDateTime);
+    }
+
+}
--- src/main/java/sample/cafekiosk/spring/domain/product/Product.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/Product.java
@@ -15,17 +15,12 @@ public class Product extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String productNumber;
-
     @Enumerated(EnumType.STRING)
     private ProductType type;
-
     @Enumerated(EnumType.STRING)
     private ProductSellingStatus sellingStatus;
-
     private String name;
-
     private int price;
 
     @Builder
