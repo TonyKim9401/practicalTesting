3cf8027d59d3e4274501bc7c2a93f5b9a0dae581
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-14 20:25:38 +0900

    July.14.2023
    Presentation Layer 테스트(1)
    
    - 외부 세계의 요청을 가장 먼저 받는 계층
    - 파라미터에 대한 최소한의 검증을 수행한다.
    
    Mock 가짜, 대역
    MockMvc
    - Mock(가짜)객체를 사용하여 스프링 MVC 동작을 재현할 수 있는 테스트 프레임워크
    
    요구사항
    - 관리자 페이지에서 신규 상품을 등록할 수 있다.
    - 상품명, 상품 타입, 판매 상태, 가격 등을 입력 받는다.

--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,26 +2,59 @@ package sample.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
 import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static sample.cafekiosk.spring.domain.product.ProductSellingStatus.*;
+import static sample.cafekiosk.spring.domain.product.ProductType.*;
+
+@Transactional
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final ProductRepository productRepository;
 
+    public ProductResponse createProduct(ProductCreateRequest request) {
+        // productNumber 부여
+        // 001 002 003 004
+        // DB 에서 마지막 저장된 Product의 상품번호를 읽어와서 +1
+        // 009 -> 010
+        String nextProductNumber = createNextProductNumber();
+
+        return ProductResponse.builder()
+                .productNumber(nextProductNumber)
+                .type(request.getType())
+                .sellingStatus(request.getSellingStatus())
+                .name(request.getName())
+                .price(request.getPrice())
+                .build();
+    }
+
+    private String createNextProductNumber() {
+        String lastedProductNumber = productRepository.findLastedProductNumber();
+        if (lastedProductNumber == null){
+            return "001";
+        }
+
+        int lastProductNumberInt = Integer.parseInt(lastedProductNumber);
+        int nextProductNumberInt = lastProductNumberInt + 1;
+        // 9 -> 009, 10 -> 010
+        return String.format("%03d", nextProductNumberInt);
+    }
     public List<ProductResponse> getSellingProducts() {
-        List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
+        List<Product> products = productRepository.findAllBySellingStatusIn(forDisplay());
         return products.stream()
                 .map(ProductResponse::of)
                 .collect(Collectors.toList());
     }
-
 
 }
