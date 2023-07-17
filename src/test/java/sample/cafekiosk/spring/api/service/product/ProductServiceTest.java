ec6625860b56ca894c7cbccf6e225b3b159e0f52
LAPTOP-RHN5RMST\ktony <k.tony9412@gmail.com>
2023-07-17 17:08:13 +0900

    July.17.2023
    한 눈에 들어오는 Test Fixture 구성하기
    
    - Fixture : 고정물, 고정되어 있는 물체
    - 테스트를 위해 원하는 상태로 고정시킨 일련의 객체
    
    given절 구성시 주로 사용
    -> 테스트시 공통적으로 사용하는 객체들
    
    1.테스트 간의 결합도를 생성 할 수 있기 때문에 지양하는 것이 좋다.
    2.테스트 메서드를 봤을때 어떤 것들이 사전에 세팅되어 있는지 확인해야 하는 번거로움이 생김. 파편화 되다 보니 문서로서의 역할이 떨어질 수 밖에 없다.
    
    data.sql같이 미리 세팅해 두는것도 마찬가지로 파편화가 일어나므로 지양하는것이 좋다. + 유지&보수 문제
    
    생성자를 통한 기본 Fixture 구성시, 명확하게 명시를 하여, 각 테스트마다 요구하는게 다를 수 있으므로 클래스마다 각각 따로 가져가는것이 좋다.
    
    Fixture가 공통사항이라 생각되어 한곳에 모아두고 만드는 것은 지양하는것이 좋다. (복잡도가 너무 올라갈 수 있다, 코틀린이라면 괜찮을지도?)

--- src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/product/ProductServiceTest.java
@@ -1,8 +1,6 @@
 package sample.cafekiosk.spring.api.service.product;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -28,6 +26,20 @@ class ProductServiceTest {
     ProductService productService;
     @Autowired
     ProductRepository productRepository;
+
+    @BeforeAll
+    static void beforeAll() {
+        // before class
+    }
+
+    @BeforeEach
+    void setUp() {
+        // before method
+
+        // 각 테스트 입장에서 봤을때 :
+        // - 아예 몰라도 테스트 내용을 이해하는 데에 문제가 없는가?
+        // - 수정해도 모든 테스트에 영향을 주지 않는가?
+    }
 
     @AfterEach
     void tearDown() {
